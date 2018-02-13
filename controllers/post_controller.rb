81b40ace9bedffdd74b268dd41883bb692db24c8
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-02-13 21:41:55 +0000

    added content to pages

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -11,8 +11,34 @@ class PostsController < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  #Landing Page
   get '/' do
   '<h1>Testing</h1>'
   end
+
+  # Posts Index Page
+  get '/post' do
+  erb :'posts/index'
+  end
+
+  # New Posts Page
+  get '/posts/new' do
+  erb :'posts/new'
+  end
+
+  # Show Page
+  get '/posts/:id' do
+
+  id = params[:id].to_i
+  erb :'posts/show'
+
+  end
+
+  # Edit Page
+  get '/posts/:id/edit' do
+
+
+  end
+
 
 end
